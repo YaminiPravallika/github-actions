8b4c5fdc32e6295db30ac53dc29ffd8c8a40e2ff
YaminiPravallika <yamini.pravallika210@gmail.com>
2022-01-19 09:47:03 +0530

    code

---
+++ src/main/java/com/example/demo/GithubCicdActions.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActions {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to javatechie !";
+	}
+
+//	echo "# github-actions-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActions.class, args);
+	}
+
+}
